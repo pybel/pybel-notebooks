aa5f2b5b29fb84c567a2ddb6d83e8d05da6a90a2
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-03-18 17:38:12 +0100

    Update testing
    Closes #4

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import os
+import unittest
 
 import click
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 
 DIRECTORY = os.path.dirname(os.path.realpath(__file__))
-
 
 # CHECK THIS http://stackoverflow.com/questions/1676269/writing-a-re-usable-parametrized-unittest-testcase-method
 
@@ -14,38 +14,19 @@ def get_all_ipynb(directory):
         if path.endswith('.ipynb'):
             yield path
 
-
-def execute_notebook(file):
-    nb = nbformat.read(file, as_version=4)
-    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
-    ep.preprocess(nb, {'metadata': {'path': 'notebooks/'}})
-
-
-@click.group()
-def main():
-    """"""
-
-
-@main.command()
-@click.option('--path', type=click.File())
-def run(path):
-    execute_notebook(path)
-
-
-@main.command()
-@click.option('--path')
-def ls(path):
-    for path in get_all_ipynb(path):
-        click.echo(path)
-
-
-@main.command()
-@click.argument('directory')
-def rund(directory):
-    for path in get_all_ipynb(directory):
-        with open(path) as f:
-            execute_notebook(f)
-
-
+class TestNotebooks(unittest.TestCase):
+    """This test class checks that Jupyter notebooks run to completion"""
+    def help(self, path):
+        with open(os.path.join(DIRECTORY, path)) as f:
+            nb = nbformat.read(file, as_version=4)
+            ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+            ep.preprocess(nb, {'metadata': {'path': DIRECTORY}})
+    
+    def test_citation_demo(self):
+        self.help('Citation Analysis.ipynb')
+        
+    def test_npa_demo(self):
+        self.help('NPA Demo.ipynb')
+        
 if __name__ == '__main__':
-    main()
+    unittest.main()
