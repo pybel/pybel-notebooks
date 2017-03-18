b7c5b38761f240bb10761762caf93386fea5fc2b
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-03-18 17:46:48 +0100

    Update formatting of test file
    [skip ci]

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from nbconvert.preprocessors import ExecutePreprocessor
 
 DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
+
 # CHECK THIS http://stackoverflow.com/questions/1676269/writing-a-re-usable-parametrized-unittest-testcase-method
 
 def get_all_ipynb(directory):
@@ -13,19 +14,22 @@ def get_all_ipynb(directory):
         if path.endswith('.ipynb'):
             yield path
 
+
 class TestNotebooks(unittest.TestCase):
     """This test class checks that Jupyter notebooks run to completion"""
+
     def help(self, path):
         with open(os.path.join(DIRECTORY, path)) as f:
             nb = nbformat.read(f, as_version=4)
             ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
             ep.preprocess(nb, {'metadata': {'path': DIRECTORY}})
-    
+
     def test_citation_demo(self):
         self.help('Citation Analysis.ipynb')
-        
+
     def test_npa_demo(self):
         self.help('NPA Demo.ipynb')
-        
+
+
 if __name__ == '__main__':
     unittest.main()
