946149dba439b133e9e39c854db8619231ecb910
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-11-20 23:51:32 +0100

    Blacken code using `forkblack`

--- test.py
+++ test.py
@@ -1,25 +1,27 @@
+import itertools as itt
 import os
 import unittest
 
 import nbformat
-from nbconvert.preprocessors import ExecutePreprocessor
 import pytest
-import itertools as itt
+from nbconvert.preprocessors import ExecutePreprocessor
 
 DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
 
 # CHECK THIS http://stackoverflow.com/questions/1676269/writing-a-re-usable-parametrized-unittest-testcase-method
 
+
 def get_all_ipynb(directory):
     for root, directory, files in os.walk(directory):
         for file in files:
-            if file.endswith('.ipynb') and 'checkpoints' not in root:
+            if file.endswith(".ipynb") and "checkpoints" not in root:
                 yield os.path.join(root, file)
 
-@pytest.mark.parametrize('path', get_all_ipynb(DIRECTORY))
+
+@pytest.mark.parametrize("path", get_all_ipynb(DIRECTORY))
 def test_notebook(path):
     with open(os.path.join(DIRECTORY, path)) as f:
         nb = nbformat.read(f, as_version=4)
-        ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
-        ep.preprocess(nb, {'metadata': {'path': DIRECTORY}})
+        ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+        ep.preprocess(nb, {"metadata": {"path": DIRECTORY}})
